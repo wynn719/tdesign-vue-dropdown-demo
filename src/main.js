838fe20f416e8df001fb72e797532eb1035fc5e0
Wynn <9843147+wynn719@users.noreply.github.com>
2025-05-21 14:43:08 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import TDesign from "tdesign-vue";
+
+// 引入组件库全局样式资源
+import "tdesign-vue/es/style/index.css";
+import "./index.css";
+import Demo from "./demo.vue";
+
+console.log("hello");
+
+Vue.use(TDesign);
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(Demo),
+}).$mount("#app");
